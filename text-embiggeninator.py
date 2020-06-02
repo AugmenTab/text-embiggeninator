bbd2a70d9cbffa2fa53c0e7be8b7424c10b7769f
AugmenTab <tbaum92@live.com>
2020-06-01 18:55:32 -0600

    Finished basic version.

--- text-embiggeninator.py
+++ text-embiggeninator.py
@@ -1,12 +1,26 @@
 #! python 3
 
-import sys
+import sys, pyperclip
 
 def embiggeninateText(string):
-    print(string)
+    embiggeninated = []
+    for character in string:
+        if character.isalpha():
+            alpha = character.lower()
+            embiggeninated.append(':regional_indicator_' + alpha + ':')
+        elif character == '!':
+            embiggeninated.append(':exclamation:')
+        elif character == '?':
+            embiggeninated.append(':question:')
+        elif character.isspace():
+            embiggeninated.append('    ')
+        else:
+            embiggeninated.append(character)
+    pyperclip.copy(' '.join(embiggeninated))
+    print('Ready for pasting!')
 
 while True:
-    print('Enter some text to embiggeninate.')
+    print('Enter some text to embiggeninate, or enter q to quit.')
     text = input()
     if text == 'q':
         print('Goodbye!')
